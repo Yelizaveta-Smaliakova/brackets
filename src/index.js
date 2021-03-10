05cd9ede3347068cd57fcc5b84aad060bcc4b040
Yelizaveta Smaliakova <smolyakova580@gmail.com>
2021-03-10 14:24:10 +0300

    feat: check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+function here(str, brackets) {
+    for (let i = 0; i < brackets.length; i++) {
+        if (str.indexOf(brackets[i]) != -1) return true;
+    }
+    return false;
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+    while (here(str, brackets)) {
+        for (let i = 0; i < brackets.length; i++) {
+            str = str.replace(brackets[i], "");
+        }
+    }
+    return (str.length == 0);
 }
