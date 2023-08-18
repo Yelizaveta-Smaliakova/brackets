de9a2153deaa378f2a0ba427bc8aef687055e1a1
ElizavetaSmolyakova <elizaveta.smolyakova@booksrun.com>
2023-08-18 16:09:13 +0300

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  brack = {
+    "()":"\\(\\)",
+    "[]":"\\[\\]",
+    "||":"\\|\\|"
+  }
+  bracketsConfig = bracketsConfig.map((el) => {
+      if (Object.keys(brack).indexOf(el.join(""))!=-1) return new RegExp(brack[el.join("")], "g")
+      else return new RegExp(el.join(""), "g")
+      });
+      
+  let oldLen = str.length
+  let newLen = 0
+
+  while (oldLen != newLen) {
+      oldLen = str.length
+      bracketsConfig.forEach((el) => str = str.replace(el,''))
+      newLen = str.length
+  }
+
+  return newLen === 0 ? true : false
 }
